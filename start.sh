f3907d29f8cd1bcc2d260d72335600bd96237f55
Alphakas <alphakas@Alphakass-iMac.local>
2021-09-15 17:50:01 +0530

    fclone config updated

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+echo "[General]
+path_to_gclone = ./fclone
+telegram_token = $telegram_token
+user_ids = $user_ids
+group_ids = $group_ids
+gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
